420121652f85fc1d9100b9a6f5c8cea1df4c24f4
LukoninDmitryPy <liondmitry4@gmail.com>
2022-03-06 16:12:36 +0300

    1.4

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,13 +12,13 @@ class AdminAuthorizedOrReadOnly(permissions.BasePermission):
 class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.method in 'GET'
+            request.method == 'GET'
             or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in 'GET'
+            request.method == 'GET'
             or request.user.is_authenticated and request.user.is_admin
             or request.user.is_moderator or request.user == obj.author
         )
